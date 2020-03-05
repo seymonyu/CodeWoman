69ae36e88f8685b72eecdee94282bb142c0d2ee4
Elsa Desta <elsamenelik@gmail.com>
2020-03-05 23:06:05 +0100

    new commit

--- src/component/ChooseMale.js
+++ src/component/ChooseMale.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import "./scss/GameOver.scss";
 
-function ChooseMale ( ) {
-return (
+function ChooseMale({ handlerOnClickMale }) {
+  return (
     <div>
-        <h1>Game Over!</h1>
+      <h1 className="game_over">Game Over!</h1>
+      <button className="btn_close" onClick={handlerOnClickMale}>
+        {" "}
+        Close
+      </button>
     </div>
-)
+  );
 }
 
 export default ChooseMale;
--- src/component/SelectHero.js
+++ src/component/SelectHero.js
@@ -4,14 +4,22 @@ import HeroList from "./HeroList";
 import Compare from "./Compare";
 import ChooseMale from "./ChooseMale";
 class SelectHero extends Component {
-  state = {
-    id_list: [589, 720, 165, 638, 309, 356, 107, 238, 643],
-    superHero: [],
-    selectHeroMount: false,
-    selectedHero: "",
-    unmount:true,
-    mountMaleComp:false
-  };
+  constructor(props){
+    super(props)
+    this.state = {
+      id_list: [589, 720, 165, 638, 309, 356, 107, 238, 643],
+      superHero: [],
+      selectHeroMount: false,
+      selectedHero: "",
+      unmount:true,
+      mountMaleComp:false
+    }; 
+    this.selectHeroOnClick = this.selectHeroOnClick.bind(this);
+    this.handlerOnClickMale = this.handlerOnClickMale.bind(this);
+    this.handlermountComp = this.handlermountComp.bind(this);
+    this.handlerOnClickMale = this.handlerOnClickMale.bind(this);
+  }
+  
 
   componentDidMount() {
     this.state.id_list.map(element => {
@@ -38,15 +46,15 @@ class SelectHero extends Component {
   }
 
   handlerOnClickMale () {
-    this.setState({mountMaleComp:true})
+    this.setState({mountMaleComp:!this.state.mountMaleComp})
     
   }
 
   render() {
     return (
       <div>
-        {this.state.mountMaleComp === true? <ChooseMale /> : null}
-        <img src= "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png" alt="place holder" onClick={this.state.mountMaleComp}/>
+        {this.state.mountMaleComp === true? <ChooseMale handlerOnClickMale={this.handlerOnClickMale} /> : null}
+        <img src= "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png" alt="place holder" onClick={this.handlerOnClickMale}/>
         {this.state.superHero.map((item, index) => (
           <HeroList key={index} obj={item} onClick={this.selectHeroOnClick} />
         ))}
