501105ab0f1977073d92cf1735e317e1fafd723a
Ligita <legyta@gmail.com>
2020-03-05 23:55:15 +0100

    evil robot branch final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import EvilRobot from "./component/EvilRobot";
 import SelectHero from "./component/SelectHero";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Start from "./component/Start";
+
 function App() {
   return (
     <div className="App">
-      <Start />
-      <SelectHero />
+      <EvilRobot />
     </div>
   );
 }
