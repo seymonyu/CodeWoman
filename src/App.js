ab5260d36005e37126bdfe56e866339e82538b7d
Ligita <legyta@gmail.com>
2020-03-05 21:22:40 +0100

    fix folder structure and merge branches start + select + compare

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
-import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Start from "./components/Start";
+import Start from "./component/Start";
 
 function App() {
   return (
     <div className="App">
       <Start />
-=======
-import Compare from "./component/Compare"
-import SelectHero from "./component/SelectHero"
-
-function App() {
-  return(
-     <div>
-     <SelectHero/>
-    
->>>>>>> 3a59c904d199a21c9ada16f26112c471e6b795a9
     </div>
-    )
-
+  );
 }
 
 export default App;
