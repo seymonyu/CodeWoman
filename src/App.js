89d1851645a55ba14fb0f2248910e5212a29245d
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-03-05 19:42:22 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
 import React from "react";
-<<<<<<< HEAD
 import SelectHero from "./component/SelectHero";
 import Compare from "./component/Compare";
 import "./App.css";
@@ -12,15 +10,5 @@ function App() {
       <Compare />
     </div>
   );
-=======
-import Compare from "./component/Compare"
-
-function App() {
-     <div>
-     <Compare />
-    </div>
-
->>>>>>> 14a187167d2fc6bc27d5f64460bdeef890bd47bf
 }
-
 export default App;
