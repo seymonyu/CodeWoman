84b598657c2a19664c33f83aef9700cff0abc1ee
anu radha <anu.sivasubramanian@gmail.com>
2020-03-05 16:59:26 +0100

    Selecting Hero

---
+++ src/component/SelectHero.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import axios from "axios";
+import HeroList from "./HeroList";
+class SelectHero extends Component {
+  state = {
+    id_list: [589, 720, 165, 638, 309, 356, 107, 238, 643],
+    superHero: [],
+    selectHeroMount: false,
+    selectedHero: ""
+  };
+
+  componentDidMount() {
+    this.state.id_list.map(element => {
+      return axios
+        .get(`http://superheroapi.com/api.php/10158157868173814/${element}`)
+        .then(response => {
+          let temp = response.data;
+          this.setState({
+            superHero: [...this.state.superHero, temp],
+            selectedHero: temp
+          });
+        })
+        .catch(error => console.error(`something went wrong: ${error}`));
+    });
+  }
+
+  selectHeroOnClick = chosenHero => {
+    this.setState({ selectedHero: chosenHero, selectHeroMount: true });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.superHero.map((item, index) => (
+          <HeroList key={index} obj={item} onClick={this.selectHeroOnClick} />
+        ))}
+      </div>
+    );
+  }
+}
+export default SelectHero;
