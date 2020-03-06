0f74340130da1b9008b72278acc6cfa556cd5e00
Elsa Desta <elsamenelik@gmail.com>
2020-03-06 09:38:10 +0100

    style 2

--- src/component/ChooseMale.js
+++ src/component/ChooseMale.js
@@ -3,12 +3,14 @@ import "./scss/GameOver.scss";
 
 function ChooseMale({ handlerOnClickMale }) {
   return (
-    <div className="gameOver_container">
+    <div className="gameOver_parent">
       <h1 className="game_over_text">Game Over!</h1>
+      <div className="btn_parent">
       <button className="btn_close" onClick={handlerOnClickMale}>
         {" "}
         Close
       </button>
+      </div>
     </div>
   );
 }
