cf95e0065c90352f07f5f7648db445d5b7c04ef0
Ligita <legyta@gmail.com>
2020-03-06 12:45:35 +0100

    Angry evil robot. to go between questions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import EvilRobot from "./component/EvilRobot";
-import SelectHero from "./component/SelectHero";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Start from "./component/Start";
-import StyleQuestions from "./component/StyleQuestions";
-import WinPopUp from "./component/WinPopUp";
-import TryAgainPopUp from "./component/TryAgainPopUp";
-import Dance from "./component/Dance"
+import AngryRobot from "./component/AngryRobot";
 
 function App() {
   return (
     <div className="App">
-      <Dance />
+      <AngryRobot />
     </div>
   );
 }
