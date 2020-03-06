53630b6ff2477f7bb3f1999e783f6d730baeb568
anu radha <anu.sivasubramanian@gmail.com>
2020-03-06 09:30:12 +0100

    integration attempt 2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import EvilRobot from "./component/EvilRobot";
 import SelectHero from "./component/SelectHero";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Start from "./component/Start";
+import Story from "./component/Start";
 
 function App() {
   return (
     <div className="App">
-      <Start />
+      {/* <Start />
       <EvilRobot />
-      <SelectHero />
+      <SelectHero /> */}
+      <Story />
     </div>
   );
 }
--- src/component/Story.js
+++ src/component/Story.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import SelectHero from "./SelectHero";
-import Compare from "./Compare";
+
 import axios from "axios";
-import Game from "./Game";
+
+import "./scss/evilRobot.scss";
+import evilRobot from "./images/evilRobot.svg";
 
 class Story extends Component {
   state = {
@@ -50,27 +52,53 @@ class Story extends Component {
 
   render() {
     return (
-      <div>
-        <p>Choose your avatar</p>
-
-        {!this.state.selectHeroMount ? null : (
-          <SelectHero
-            superHero={this.state.superHero}
-            selectHeroUnmount={this.handlerUnmountSelectHero}
-            selectHero={this.selectHeroOnClick}
-          />
-        )}
-        <button onClick={this.handlerUnmountSelectHero}>Hero Select</button>
-        {!this.state.gameUnmount ? null : (
-          <Game
-            id_list={this.state.id_list}
-            selectedHero={this.state.selectedHero}
-            gameUnmount={this.handlerUnmountGame}
-            ourHeroUrl={this.state.ourHero}
-          />
-        )}
-        <button onClick={this.handlerUnmountGame}>Enter Game</button>
-      </div>
+      <section className="robot">
+        <div className="robot--wrap container-fluid">
+          <div className="robot--row row">
+            <div className="robot--col_left col">
+              <div className="robot--wrapper">
+                <img className="robot--eye" src={evilRobot} alt="Eye" />
+              </div>
+            </div>
+            <div className="robot--col_right col">
+              <div className="speech-bubble">
+                <div className="typewriter-first">
+                  <h1>I have all my heroes fighting for me.</h1>
+                </div>
+                <div className="typewriter-second">
+                  <h1>You have no chance.</h1>
+                </div>
+                <div className="typewriter-third">
+                  <h1>You will never have a job in tech!</h1>
+                </div>
+                <div className="typewriter-fourth">
+                  <h1>Hahahahahahahahahahahahaha.</h1>
+                </div>
+                <div className="typewriter-fifth">
+                  <h1>What?? You wanna fight me??</h1>
+                </div>
+              </div>
+              <div className="circle--wrap">
+                <div className="blob"></div>
+                <button
+                  type="button"
+                  className="circle--typo btn btn-link"
+                  onClick={this.handlerUnmountSelectHero}
+                >
+                  Choose a heroine
+                </button>
+                {!this.state.selectHeroMount ? null : (
+                  <SelectHero
+                    superHero={this.state.superHero}
+                    selectHeroUnmount={this.handlerUnmountSelectHero}
+                    selectHero={this.selectHeroOnClick}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
