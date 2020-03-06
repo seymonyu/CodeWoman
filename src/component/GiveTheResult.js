56a1301fabfae152bdabd882b04deeee01ab3d6c
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-03-06 13:31:13 +0100

    seyma

--- src/component/GiveTheResult.js
+++ src/component/GiveTheResult.js
@@ -1,12 +1,13 @@
 import React from "react";
 import WinPopUp from "./WinPopUp";
+import TryAgainPopUp from "./TryAgainPopUp";
 
 function GiveTheResult({ flag, handlerunmount, mount }) {
   return (
     <div>
       {flag === 1 ? <WinPopUp handlerunmount={handlerunmount} /> : null}
 
-      {flag === 2 ? <WinPopUp handlerunmount={handlerunmount} /> : null}
+      {flag === 2 ? <TryAgainPopUp handlerunmount={handlerunmount} /> : null}
     </div>
   );
 }
