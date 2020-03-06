e8836a0fa898f1d47cc66b1b2f4cfc36aed0bf97
anu radha <anu.sivasubramanian@gmail.com>
2020-03-06 10:07:49 +0100

    Seyma's workable version

--- src/component/Story.js
+++ src/component/Story.js
@@ -3,8 +3,9 @@ import SelectHero from "./SelectHero";
 
 import axios from "axios";
 
-import "./scss/evilRobot.scss";
+// import "./scss/evilRobot.scss";
 import evilRobot from "./images/evilRobot.svg";
+import Game from "./Game";
 
 class Story extends Component {
   state = {
@@ -52,53 +53,66 @@ class Story extends Component {
 
   render() {
     return (
-      <section className="robot">
-        <div className="robot--wrap container-fluid">
-          <div className="robot--row row">
-            <div className="robot--col_left col">
-              <div className="robot--wrapper">
-                <img className="robot--eye" src={evilRobot} alt="Eye" />
+      <div>
+        {this.state.selectHeroMount ? (
+          // <SelectHero
+          //   superHero={this.state.superHero}
+          //   selectHeroUnmount={this.handlerUnmountSelectHero}
+          //   selectHero={this.selectHeroOnClick}
+          //   id_list={this.state.id_list}
+          //   selectedHero={this.state.selectedHero}
+          //   gameUnmount={this.handlerUnmountGame}
+          //   ourHeroUrl={this.state.ourHero}
+          // />
+          <Game
+            id_list={this.state.id_list}
+            selectedHero={this.state.selectedHero}
+            gameUnmount={this.handlerUnmountGame}
+            ourHeroUrl={this.state.ourHero}
+          />
+        ) : (
+          <section className="robot">
+            <div className="robot--wrap container-fluid">
+              <div className="robot--row row">
+                <div className="robot--col_left col">
+                  <div className="robot--wrapper">
+                    <img className="robot--eye" src={evilRobot} alt="Eye" />
+                  </div>
+                </div>
+                <div className="robot--col_right col">
+                  <div className="speech-bubble">
+                    <div className="typewriter-first">
+                      <h1>I have all my heroes fighting for me.</h1>
+                    </div>
+                    <div className="typewriter-second">
+                      <h1>You have no chance.</h1>
+                    </div>
+                    <div className="typewriter-third">
+                      <h1>You will never have a job in tech!</h1>
+                    </div>
+                    <div className="typewriter-fourth">
+                      <h1>Hahahahahahahahahahahahaha.</h1>
+                    </div>
+                    <div className="typewriter-fifth">
+                      <h1>What?? You wanna fight me??</h1>
+                    </div>
+                  </div>
+                  <div className="circle--wrap">
+                    <div className="blob"></div>
+                    <button
+                      type="button"
+                      className="circle--typo btn btn-link"
+                      onClick={this.handlerUnmountSelectHero}
+                    >
+                      Choose a heroine
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="robot--col_right col">
-              <div className="speech-bubble">
-                <div className="typewriter-first">
-                  <h1>I have all my heroes fighting for me.</h1>
-                </div>
-                <div className="typewriter-second">
-                  <h1>You have no chance.</h1>
-                </div>
-                <div className="typewriter-third">
-                  <h1>You will never have a job in tech!</h1>
-                </div>
-                <div className="typewriter-fourth">
-                  <h1>Hahahahahahahahahahahahaha.</h1>
-                </div>
-                <div className="typewriter-fifth">
-                  <h1>What?? You wanna fight me??</h1>
-                </div>
-              </div>
-              <div className="circle--wrap">
-                <div className="blob"></div>
-                <button
-                  type="button"
-                  className="circle--typo btn btn-link"
-                  onClick={this.handlerUnmountSelectHero}
-                >
-                  Choose a heroine
-                </button>
-                {!this.state.selectHeroMount ? null : (
-                  <SelectHero
-                    superHero={this.state.superHero}
-                    selectHeroUnmount={this.handlerUnmountSelectHero}
-                    selectHero={this.selectHeroOnClick}
-                  />
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        )}
+      </div>
     );
   }
 }
