d1ed1d34ca92d90d03396dcabf879e6851c39db5
Elsa Desta <elsamenelik@gmail.com>
2020-03-06 12:26:25 +0100

    style

--- src/component/ChooseMale.js
+++ src/component/ChooseMale.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import "./scss/GameOver.scss";
+import giphy from "./giphy.gif";
 
 function ChooseMale({ handlerOnClickMale }) {
   return (
-    <div className="gameOver_parent">
+    <div className="gameOver_parent">      
+    
+    
       <h1 className="game_over_text">Game Over!</h1>
-      <div className="btn_parent">
-      <button className="btn_close" onClick={handlerOnClickMale}>
-        {" "}
-        Close
-      </button>
+      <div className="btn_parent_gameover">
+        <button className="btn_close_gameover" onClick={handlerOnClickMale}>
+          {" "}
+          Close
+        </button>
       </div>
     </div>
   );
