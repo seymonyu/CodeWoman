afda552432aace4133aab6db6aec9c578d68bcd3
Ligita <legyta@gmail.com>
2020-03-06 09:55:50 +0100

    Style page for questions

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import EvilRobot from "./component/EvilRobot";
 import SelectHero from "./component/SelectHero";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Start from "./component/Start";
+import StyleQuestions from "./component/StyleQuestions";
 
 function App() {
   return (
     <div className="App">
-      <Start />
-      <EvilRobot />
-      <SelectHero />
+      <StyleQuestions />
     </div>
   );
 }
